34369ede969b6d78e4053370d627e062613df266
Irum <irum@Irums-MacBook-Air.local>
2018-12-23 22:24:51 +0000

    Done.

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,25 +1,56 @@
 import React from "react";
+import { runInThisContext } from "vm";
 
 class LoginForm extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      username: "",
+      password: ""
+    };
   }
+
+  handleChange = event => {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    this.state.username && this.state.password ? (
+      this.props.onSubmit
+    ) : (
+      <p> Both username and password must be entered </p>
+    );
+  };
 
   render() {
     return (
-      <form>
+      <form onSubmit={event => this.handleSubmit(event)}>
         <div>
           <label>
             Username
-            <input id="username" name="username" type="text" />
+            <input
+              id="username"
+              name="username"
+              value={this.state.username}
+              onChange={this.handleChange}
+              type="text"
+            />
           </label>
         </div>
         <div>
           <label>
             Password
-            <input id="password" name="password" type="password" />
+            <input
+              id="password"
+              name="password"
+              value={this.state.password}
+              onChange={this.handleChange}
+              type="password"
+            />
           </label>
         </div>
         <div>
@@ -31,3 +62,33 @@ class LoginForm extends React.Component {
 }
 
 export default LoginForm;
+
+// 2. This component takes one prop: `onSubmit` which is a function — this function
+//   is called when the form is being submitted.By default, this function is
+// currently just includes`console.log` that will allow you to see if the form is
+// functioning correctly when working in your browser.
+
+// 3. You'll find two inputs in this component: `<input type="text">` and `<input
+// type = "password" > `. Make this a controlled component by adding the necessary
+// attributes to these inputs. The input values should be saved to the
+// component's state, again on every change.
+
+// 4. An example of an input would look like:
+
+//    ```js
+//   < input
+// id = "test-username"
+// type = "text"
+// name = "username"
+// value = { this.state.username }
+// onChange = { this.handleInputChange }
+//   />
+//   ```
+
+// 5. Remember that you can retrieve the input `name` and `value` of an
+// `event.target` from the JS event.
+
+// 6. Add the necessary event handler to the `<form>` element in order to call the `onSubmit` callback prop.
+
+// 7. The `onSubmit` callback prop should only be called if _both_ fields are
+// filled in (with any value).
--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -4,17 +4,42 @@ class TwitterMessage extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      tweet: ""
+    };
   }
+
+  handleTweetChange = event => {
+    this.setState({
+      tweet: event.target.value
+    });
+  };
 
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" />
+        <input
+          type="text"
+          onChange={event => this.handleTweetChange(event)}
+          value={this.state.tweet}
+        />
+        {this.props.maxChars - this.state.tweet.length}
       </div>
     );
   }
 }
 
 export default TwitterMessage;
+
+// This component takes one prop: maxChars which is a number — the maximum amount
+// of characters a message can have.This prop is being passed in from the App
+// component with the value 140.
+
+// You'll find an <input type="text"> in this component. Make this a controlled
+// component by adding the attributes to the <input> element. Its value should be
+// saved in the component's state and should update on every change to the input.
+
+// Show the remaining characters in the component.It doesn't matter how you render
+// it, as long as the number is correct. No need to guard against input that is
+// too long — you can let the counter reach negative values.
