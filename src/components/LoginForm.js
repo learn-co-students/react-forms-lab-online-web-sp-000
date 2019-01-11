7be117d173e6528a196bef32d120fc8d1c9e87a9
danmj <daniel.jung@stern.nyu.edu>
2019-01-11 09:42:06 -0500

    Done.

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,25 +1,41 @@
 import React from "react";
 
-class LoginForm extends React.Component {
+export default class LoginForm extends React.Component {
   constructor() {
-    super();
+    super()
+    this.state = {
+      username: '',
+      password: ''
+    }
+  }
 
-    this.state = {};
+  handleChange = event => {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+
+  handleSubmit = event => {
+    event.preventDefault()
+
+    if (!this.state.username || !this.state.password) return
+
+    this.props.onSubmit(this.state)
   }
 
   render() {
     return (
-      <form>
+      <form onSubmit={this.handleSubmit}>
         <div>
           <label>
             Username
-            <input id="username" name="username" type="text" />
+            <input id="username" name="username" type="text" onChange={this.handleChange} />
           </label>
         </div>
         <div>
           <label>
             Password
-            <input id="password" name="password" type="password" />
+            <input id="password" name="password" type="password" onChange={this.handleChange} />
           </label>
         </div>
         <div>
@@ -29,5 +45,3 @@ class LoginForm extends React.Component {
     );
   }
 }
-
-export default LoginForm;
--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -1,20 +1,20 @@
 import React from "react";
 
-class TwitterMessage extends React.Component {
+export default class TwitterMessage extends React.Component {
   constructor() {
-    super();
-
-    this.state = {};
+    super()
+    this.state = {
+      message: ''
+    }
   }
 
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" />
+        <input type="text" onChange={event => this.setState({message: event.target.value})} value={this.state.message}/>
+        {this.props.maxChars-this.state.message.length}
       </div>
-    );
+    )
   }
 }
-
-export default TwitterMessage;
