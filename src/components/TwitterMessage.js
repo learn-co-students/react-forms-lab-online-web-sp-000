5c489612aa73867fb3ed7a02db9c3c47355eae4f
betodute <betodute@gmail.com>
2020-01-17 16:33:44 -0700

    Controlled Forms with Validations in Event Handler

--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -1,17 +1,29 @@
 import React from "react";
 
 class TwitterMessage extends React.Component {
-  constructor() {
+  constructor(props) {
     super();
 
-    this.state = {};
+    this.state = {
+      tweet: "",
+      characters: props.maxChars
+    };
+  }
+
+  handleTweet = event => {
+    console.log(event.target.value)
+    this.setState({
+      message: event.target.value,
+      characters: this.props.maxChars - event.target.value.length
+    })
   }
 
   render() {
+    console.log(this.state.message)
     return (
       <div>
-        <strong>Your message:</strong>
-        <input type="text" name="message" id="message" />
+        <strong>You have {this.state.characters} characters remainings.</strong>
+        <input type="text" name="message" id="message" onChange={this.handleTweet} value={this.state.message} />
       </div>
     );
   }
