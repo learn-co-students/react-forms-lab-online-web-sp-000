3c44b2849c37d944c29c45f1967779914b180c40
Mark <m.hern1415@gmail.com>
2020-05-11 15:26:45 -0700

    Done.

--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -25,14 +25,9 @@ class TwitterMessage extends React.Component {
         <div>
           <strong>Your message:</strong>
           <input type="text" name="message" id="message" value={this.state.inputValue} onChange={this.handleChange}/>
-          {/* Is there a maximum length for an input in a React form. 
-          Need to put that here and assign it the prop value */}
         </div>
         <p>{this.state.remainingChars}</p>
       </React.Fragment>
-      // to show the number of remaining characters available, 
-      // need to call a method here that takes the maxChars prop and 
-      // subtracts the length of the input value string
     );
   }
 }
