299fa66a3d1c51093abb4f3285522dc14fecd17e
jkellyphilly <joel.kelly.philly@gmail.com>
2020-06-17 13:53:11 -0400

    Done.

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -4,22 +4,51 @@ class LoginForm extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      username: '',
+      password: ''
+    };
+  }
+
+  handleInputChange = event => {
+    this.setState({
+      [event.target.name] : event.target.value
+    })
   }
 
   render() {
     return (
-      <form>
+      <form onSubmit={event => {
+          event.preventDefault()
+          if((this.state.username === '') || (this.state.password === '')) {
+            console.log('Error: username and password cannot be blank')
+          } else {
+            this.props.handleLogin(this.state)
+          }
+        }
+      }>
         <div>
           <label>
             Username
-            <input id="username" name="username" type="text" />
+            <input
+              id="username"
+              name="username"
+              type="text"
+              value={this.state.username}
+              onChange={this.handleInputChange}
+            />
           </label>
         </div>
         <div>
           <label>
             Password
-            <input id="password" name="password" type="password" />
+            <input
+              id="password"
+              name="password"
+              type="password"
+              value={this.state.password}
+              onChange={this.handleInputChange}
+            />
           </label>
         </div>
         <div>
