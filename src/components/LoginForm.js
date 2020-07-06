46fc058ae44a2aa5e98dbfb073f650017d34724d
Lauren McCoy <lm_lutec@outlook.com>
2020-07-06 11:59:30 -0400

    Done.

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -4,22 +4,42 @@ class LoginForm extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      username: "",
+      password: ""
+    };
+  }
+
+  updateUsername = (event) => {
+    this.setState({
+      username: event.target.value
+    })
+  }
+
+  updatePassword = (event) => {
+    this.setState({
+      password: event.target.value
+    })
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault()
+    
   }
 
   render() {
     return (
-      <form>
+      <form onSubmit={event => this.handleSubmit(event)}>
         <div>
           <label>
             Username
-            <input id="username" name="username" type="text" />
+            <input id="username" name="username" type="text" value={this.state.username} onChange={event => this.updateUsername(event)} />
           </label>
         </div>
         <div>
           <label>
             Password
-            <input id="password" name="password" type="password" />
+            <input id="password" name="password" type="password" value={this.state.password} onChange={event => this.updatePassword(event)}/>
           </label>
         </div>
         <div>
--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -1,20 +1,33 @@
 import React from "react";
 
 class TwitterMessage extends React.Component {
-  constructor() {
-    super();
 
-    this.state = {};
+  state = {
+    message: ""
+  }
+
+
+  changeMaxChars = event => {
+    this.setState({
+      message: event.target.value
+    })
+  }
+
+  charCounter = () => {
+    return this.props.maxChars - this.state.message.length
   }
 
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" name="message" id="message" />
+        <input onChange={event => { this.changeMaxChars(event)}} value={this.state.message} type="text" name="message" id="message" />
+        {this.charCounter()}
       </div>
     );
   }
 }
 
 export default TwitterMessage;
+
+
