ed91f8a283aa899e6c9cb10e65bb0dd4d5292928
github username <github email address>
2020-07-28 20:22:52 -0400

    Done.

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,33 +1,62 @@
 import React from "react";
 
 class LoginForm extends React.Component {
-  constructor() {
-    super();
+    constructor() {
+        super();
 
-    this.state = {};
-  }
+        this.state = {
+            username: '',
+            password: ''
+        };
+    }
 
-  render() {
-    return (
-      <form>
-        <div>
-          <label>
-            Username
-            <input id="username" name="username" type="text" />
-          </label>
-        </div>
-        <div>
-          <label>
-            Password
-            <input id="password" name="password" type="password" />
-          </label>
-        </div>
-        <div>
-          <button type="submit">Log in</button>
-        </div>
-      </form>
-    );
-  }
+    handleChange = event => {
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+    }
+
+    handleSubmit = event => {
+        event.preventDefault()
+
+        if (!this.state.username || !this.state.password) return
+
+        this.props.handleLogin(this.state)
+    }
+
+    render() {
+        return ( <
+            form onSubmit = { this.handleSubmit } >
+            <
+            div >
+            <
+            label >
+            Username <
+            input id = "username"
+            name = "username"
+            type = "text"
+            onChange = { this.handleChange }
+            /> <
+            /label> <
+            /div> <
+            div >
+            <
+            label >
+            Password <
+            input id = "password"
+            name = "password"
+            type = "password"
+            onChange = { this.handleChange }
+            /> <
+            /label> <
+            /div> <
+            div >
+            <
+            button type = "submit" > Log in < /button> <
+            /div> <
+            /form>
+        );
+    }
 }
 
 export default LoginForm;
