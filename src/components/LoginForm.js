9c16e928dde5b4487c98c14790e2614b9f105401
batsaikhan84 <u.batsaikhan@gmail.com>
2020-08-21 01:50:57 -0500

    Done.

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -4,22 +4,43 @@ class LoginForm extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      username: '',
+      password: ''
+    };
+  }
+  on_submit = (event) => {
+    event.preventDefault()
+    if (this.state.username !== '' && this.state.password !== '') {
+      this.props.handleLogin()
+    }
+  }
+  get_username = (event) => {
+    this.setState({
+      username: event.target.value
+    })
+
+  }
+  get_password = (event) => {
+    this.setState({
+      password: event.target.value
+    })
+
   }
 
   render() {
     return (
-      <form>
+      <form  onSubmit={event => this.on_submit(event)}>
         <div>
           <label>
             Username
-            <input id="username" name="username" type="text" />
+            <input id="username" name="username" type="text" onChange={event => this.get_username(event)} value={this.state.username}/>
           </label>
         </div>
         <div>
           <label>
             Password
-            <input id="password" name="password" type="password" />
+            <input id="password" name="password" type="password" onChange={event => this.get_password(event)} value={this.state.password}/>
           </label>
         </div>
         <div>
--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -4,14 +4,28 @@ class TwitterMessage extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {value: ""};
+  }
+  updateState = (event) => {
+    this.setState({
+      value: event.target.value
+    })
+  }
+  counter = () => {
+    console.log()
+    if (typeof this.state.value === 'undefined') {
+      return this.props.maxChars
+    } else {
+      return this.props.maxChars - (this.state.value).length
+    }
   }
 
   render() {
     return (
       <div>
+        <h1>{this.counter()}</h1>
         <strong>Your message:</strong>
-        <input type="text" name="message" id="message" />
+        <input type="text" name="message" id="message" onChange={event => this.updateState(event)} value={this.state.value}/>
       </div>
     );
   }
