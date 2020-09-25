dfff02ab7d79d779c3142525489f36a21092e0f1
github username <github email address>
2020-09-25 19:50:50 -0400

    Done.

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -4,22 +4,51 @@ class LoginForm extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      username: '',
+      password: ''
+    };
   }
+
+  handleUsernameInput = (event) => {
+    this.setState({
+      ...this.state,
+      username: event.target.value //retrieve the input name and value 
+    })
+  }
+
+  handlePasswordInput = (event) => {
+    this.setState({
+      ...this.state,
+      password: event.target.value
+    })
+  }
+
+  //prop 
+  handleLogin = (event) => {
+    event.preventDefault();
+    if (this.state.password && this.state.username){
+      this.props.handleLogin(this.state) 
+    }
+  }
+
+  //prop called when the form is being submitted 
+  //add attributes to input (password,usernme,text)
+  //event handler:onSubmit callback prop
 
   render() {
     return (
-      <form>
+      <form onSubmit={this.handleLogin}> 
         <div>
           <label>
             Username
-            <input id="username" name="username" type="text" />
+            <input id="username" name="username" type="text" value={this.state.username} onChange={this.handleUsernameInput} />
           </label>
         </div>
         <div>
           <label>
             Password
-            <input id="password" name="password" type="password" />
+            <input id="password" name="password" type="password" value={this.state.password} onChange={this.handlePasswordInput}/>
           </label>
         </div>
         <div>
--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -1,20 +1,38 @@
 import React from "react";
 
 class TwitterMessage extends React.Component {
-  constructor() {
-    super();
+  constructor(props) { //takes one prop 
+    super(props);
 
-    this.state = {};
+    this.state = {
+      inputValue: '', 
+      maxlength: this.props.maxChars, //maxChars = prop (maximum amount of characteres a message can have)
+      remainingChars: this.props.maxChars //being passed in from app component
+    };
   }
 
+  handleChange = (event) => {
+    this.setState({
+      inputValue: event.target.value, 
+      remainingChars: this.props.maxChars - event.target.value.length
+    })
+  }
+
+  //input type controlled component 
+  //change on every input 
+  //show remaining characters 
   render() {
+    console.log(this.state.inputValue)
     return (
-      <div>
-        <strong>Your message:</strong>
-        <input type="text" name="message" id="message" />
-      </div>
+      <React.Fragment>
+        <div>
+          <strong>Your message:</strong> 
+          <input type="text" name="message" id="message" value={this.state.inputValue} onChange={this.handleChange}/>
+        </div>
+        <p>{this.state.remainingChars}</p> 
+      </React.Fragment>
     );
   }
 }
 
-export default TwitterMessage;
+export default TwitterMessage; 
