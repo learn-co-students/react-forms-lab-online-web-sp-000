4b684e1a84aca366f2fd3a14732647735dd77119
Emily Gann <aspentree79@gmail.com>
2021-01-27 09:35:40 -0500

    Done.

--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -4,14 +4,27 @@ class TwitterMessage extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      message: ''
+    };
   }
 
+  handleCharacters = event => {
+    this.setState({
+      message: event.target.value
+    })
+  }
+
+
+
   render() {
+    const charNum = this.props.maxChars - this.state.message.length;
+
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" name="message" id="message" />
+        <input type="text" name="message" id="message" onChange={this.handleCharacters} value={this.state.message} />
+        {charNum}
       </div>
     );
   }
