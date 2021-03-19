7f7551f62c464fbaebed858aac17f2230d8ff951
Scott Hoisington <hoyzington@gmail.com>
2021-03-18 20:11:55 -0400

    Done.

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,33 +1,62 @@
-import React from "react";
+import React from "react"
 
 class LoginForm extends React.Component {
   constructor() {
-    super();
+    super()
+    this.state = {
+      username: '',
+      password: ''
+    }
+  }
 
-    this.state = {};
+  handleChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+    const username = this.state.username
+    const password = this.state.password
+    if (username.length > 0 && password.length > 0) {
+      this.props.handleLogin({username, password})
+    }
   }
 
   render() {
     return (
-      <form>
+      <form onSubmit={e => this.handleSubmit(e)} >
         <div>
           <label>
-            Username
-            <input id="username" name="username" type="text" />
+            Username&nbsp;
+            <input
+              id="username"
+              name="username"
+              type="text"
+              onChange={e => this.handleChange(e)}
+              value={this.state.username}
+            />
           </label>
         </div>
         <div>
           <label>
-            Password
-            <input id="password" name="password" type="password" />
+            Password&nbsp;
+            <input
+              id="password"
+              name="password"
+              type="password"
+              onChange={e => this.handleChange(e)}
+              value={this.state.password}
+            />
           </label>
-        </div>
+        </div><br/>
         <div>
           <button type="submit">Log in</button>
         </div>
       </form>
-    );
+    )
   }
 }
 
-export default LoginForm;
+export default LoginForm
